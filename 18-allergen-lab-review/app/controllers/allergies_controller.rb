0bfd34af2139bc46b2e47182cde149458589c50c
github username <github email address>
2020-02-04 13:19:36 -0500

    Added code from the Allergen review session.

---
+++ 18-allergen-lab-review/app/controllers/allergies_controller.rb
@@ -0,0 +1,7 @@
+class AllergiesController < ApplicationController
+  def new
+  end
+
+  def create
+  end
+end
---
+++ 18-allergen-lab-review/app/models/ingredient.rb
@@ -0,0 +1,7 @@
+class Ingredient < ApplicationRecord
+  has_many :allergies 
+  has_many :users, through: :allergies
+
+  has_many :recipe_ingredients
+  has_many :recipes, through: :recipe_ingredients
+end
