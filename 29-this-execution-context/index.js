479040e7e38e97b1cdaf101c2cc8978707c54391
Shivang <shivang.dave@flatironschool.com>
2020-02-26 14:18:43 -0500

    add this && execution context..

--- 29-this-execution-context/index.js
+++ 29-this-execution-context/index.js
@@ -1,28 +1,32 @@
+// const dog = {
+//   name: 'winfield',
+//   favSnacks: [
+//     'cabbage', 'carrots', 'bones'
+//   ],
+//   eatSnacks: function() {
+//     // this => dog
+//     this.favSnacks.forEach(snack => {
+//       console.log(`${this.name} is eating ${snack}`)
+//     })
+//   },
+//   eatSnacksAllFunction: function() {
+//     // this => dog
+//     this.favSnacks.forEach(someCallBackWhatever.bind(this))
+//   }
+// }
+//
+// function someCallBackWhatever(snack) {
+//   console.log(`${this.name} is eating ${snack}`)
+// }
+//
+//
 
-const dog = {
-  name: 'winfield',
-  favSnacks: [
-    'cabbage', 'carrots', 'bones'
-  ],
-  eatSnacks: function() {
-    this.favSnacks.forEach(snack => {
-      console.log(`${this.name} is eating ${snack}`)
-    })
-  },
-  eatSnacksAllFunction: function() {
-    this.favSnacks.forEach(function(snack) {
-      console.log(`${this.name} is eating ${snack}`)
-    })
-  }
-}
 
-function someCallBackWhatever(snack) {
-  console.log(`${this.name} is eating ${snack}`)
-}
 
 const seal = {
   name: 'sealy',
   kiss: function() {
+    debugger;
     console.log(`${this.name} from a rose`)
   }
 }
@@ -30,72 +34,3 @@ const seal = {
 seal.kiss()
 
 var someMethod = seal.kiss
-
-
-// const dog = {
-//   name: 'winfield',
-//   favSnacks: ['cabbage', 'carrots', 'bones'],
-//   arrowEatSnacks: function() {
-//     this.favSnacks.forEach( snack => {
-//       console.log(`${this.name} is eating ${snack}`)
-//     })
-//   },
-//   regEatSnacks: function() {
-//     this.favSnacks.forEach(function(snack) {
-//       console.log(`${this.name} is eating ${snack}`)
-//     }.bind(person))
-//   }
-// }
-
-// console.log('this is the function keyword', dog.regEatSnacks())
-// console.log('this is the arrow', dog.arrowEatSnacks())
-//
-// console.log(filtered)
-// this.name = 'window object'
-
-
-//
-// // /************ Function Called with New Keyword ***********/
-// // //inside a constructor fn, this will be the newly created object
-// //
-
-// const personConstructor = function(name) {
-//   // `this` within a constructor will be the newly created object
-//   // { name: name }
-//   this.name = name
-// }
-//
-
-// const evans = new personConstructor('evans')
-//
-// function onTheGlobal() {
-//   console.log(this)
-// }
-//
-// const instructor = {
-//   name: "POWERFUL MC",
-//   lecture: function(){
-//     return `${this.name} gives POWERFUL lectures ;)`
-//   }
-// }
-//
-
-
-/*************************************************************************************************************************/
-  /* Custom forEach */
-/*************************************************************************************************************************/
-
-    function myForEach(coolArray, callback){
-      for (var i = 0; i < coolArray.length; i++) {
-        callback(coolArray[i])
-      }
-    }
-
-    function myCoolSuperAwesomeMegaCB(name) {
-      console.log(name + "!")
-    }
-
-    const instructors = ['Maddie', 'Hannah', 'Princeton', 'Kaeland']
-
-    myForEach(instructors, myCoolSuperAwesomeMegaCB)
-/*************************************************************************************************************************/
--- 29-this-execution-context/self.rb
+++ 29-this-execution-context/self.rb
@@ -1,26 +1,23 @@
 class Person
+
   # attr_accessor :name
+
   def initialize(name)
     @name = name
+    # puts "#{self.name}"
   end
 
-  def Person.say_something
+  def self.say_something
+    # self?????
+    puts "#{self}"
     puts "I am a class method defined on the #{self} class."
   end
 
   def name
-    self???
     @name
   end
 end
 
-Person.say_something # => self will be Person class
-# self in ruby is the receiver of a method
-#
-andy = Person.new('andy')
+# Person.say_something
+# andy = Person.new('andy')
 andy.name
-
-
-# => self ?
-# (self will be the instance)
-# andy (instance) is receiving the method call
